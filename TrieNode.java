832e5e0138838106dd75a665071f970de14d8ce4
nao1000 <oswaldna123@gmail.com>
2024-04-06 03:17:11 -0700

    Updated node class with new attributes

--- TrieNode.java
+++ TrieNode.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class TrieNode {
 
     private boolean terminal;
@@ -5,6 +7,8 @@ public class TrieNode {
     private TrieNode[] nextChars;
     private int nextCount;
     private TrieNode parent;
+    private ArrayList<Integer> terminalIndexes;
+    private ArrayList<Integer> charIndexes;
 
     public TrieNode(char inKey, TrieNode parent) {
         key = inKey;
@@ -12,14 +16,33 @@ public class TrieNode {
         nextCount = 0;
         terminal = false;
         this.parent = parent;
+        terminalIndexes = new ArrayList<Integer>();
+        charIndexes = new ArrayList<Integer>();
+        if (parent != null ) {
+            parent.trackChar(inKey);
+        }
+    }
+
+    public void trackChar(char inKey) {
+        charIndexes.add(inKey - 'a');
     }
 
     public void setTerminal() {
         terminal = true;
+        parent.trackTerminal(key);
+    }
+
+    public void trackTerminal(char inKey) {
+        terminalIndexes.add(inKey - 'a');
     }
 
     public void unsetTerminal() {
+        parent.forgetTerminal(key);
         terminal = false;
+    }
+
+    public void forgetTerminal(char inKey) {
+        terminalIndexes.remove(inKey - 'a');
     }
 
     public boolean isWord() {
@@ -40,6 +63,10 @@ public class TrieNode {
 
     public void incrCount() {
         nextCount++;
+    }
+
+    public void decCount() {
+        nextCount--;
     }
 
     public TrieNode jumpUp() {
